58ac5e9e1f5ad7e7b2ddd5227f3fd93edcb4d8cb
fwjok <fwjok@163.com>
2016-07-20 21:35:12 +0800

    完成了基本的加载地图的功能

---
+++ GisSet/UserControl1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace GisSet
+{
+    partial class GisSetControl
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addlayer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // addlayer
+            // 
+            this.addlayer.Location = new System.Drawing.Point(95, 73);
+            this.addlayer.Name = "addlayer";
+            this.addlayer.Size = new System.Drawing.Size(75, 23);
+            this.addlayer.TabIndex = 0;
+            this.addlayer.Text = "加载地图";
+            this.addlayer.UseVisualStyleBackColor = true;
+            this.addlayer.Click += new System.EventHandler(this.addlayer_Click);
+            // 
+            // GisSetControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.addlayer);
+            this.Name = "GisSetControl";
+            this.Size = new System.Drawing.Size(407, 239);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addlayer;
+    }
+}
---
+++ GisSet/UserControl1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AxMapWinGIS;
+using MapWinGIS;
+
+namespace GisSet
+{
+    public partial class GisSetControl: UserControl
+    {
+        public GisSetControl()
+        {
+            InitializeComponent();
+        }
+
+        //地图控件的实例化
+        private AxMap map;
+        //加载地图的地址
+        private string MapPath;
+
+        public AxMap MAP
+        {
+            get
+            {
+                return map;
+            }
+            set
+            {
+                map = value;
+            }
+        }
+
+        private void addlayer_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog folder = new System.Windows.Forms.FolderBrowserDialog();
+
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                MapPath = folder.SelectedPath;
+                AddLayerToMap(map);
+            }
+        }
+
+        private int AddLayerToMap(AxMap MapSet)
+        {
+            return MapSet.AddLayerFromFilename(MapPath, tkFileOpenStrategy.fosAutoDetect, true);
+        }
+    }
+}
